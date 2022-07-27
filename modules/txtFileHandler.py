47308183a8a2a194500d4596249a40b2cade0dd4
Micheal Nestor <michealnestor@outlook.com>
2022-07-27 10:23:10 +0000

    Added some error handling

--- modules/txtFileHandler.py
+++ modules/txtFileHandler.py
@@ -1,11 +1,14 @@
 # txtFileHandler.py
 # this handles reading urls from txt files
 from modules.scrapeURL import scrapeURL
+from modules.returnCodes import *
 
 def TXTHandler(file):
     try:
         with open(file) as file:
             for url in file.readlines():
-                scrapeURL(url)
+                returnCode = scrapeURL(url)
+                if returnCode == WEBDRIVER_ERROR:
+                    return WEBDRIVER_ERROR
     except FileNotFoundError:
         print(f"ERROR: {file} NOT FOUND")
